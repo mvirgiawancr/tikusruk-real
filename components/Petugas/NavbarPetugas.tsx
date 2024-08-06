0aa55cbebae03a4042ab4cfcf5a3c044e17bd502
Moch Virgiawan Caesar Ridollohi <109588365+mvirgiawancr@users.noreply.github.com>
2024-08-06 20:56:46 +0700

    Create NavbarPetugas.tsx

---
+++ components/Petugas/NavbarPetugas.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-primary text-base-200 flex justify-between h-[80px]">
+      <div className=" flex flex-col">
+        <Link href={"#"} className="btn btn-ghost text-xl">
+          Tikusruk
+        </Link>
+        <p className="text-sm">Tiket Bus Terhubung Koneksi</p>
+      </div>
+      <div className="navbar-end">
+        <Link href={"/"} className="btn">
+          Log Out
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
